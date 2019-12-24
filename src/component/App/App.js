c943964b05df9672ed266e3d4c05e9d030b5ec3d
tataqt <tata_98@ukr.net>
2019-12-24 16:00:55 +0200

    compleate

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from '../UserList/UserList';
-import { PostList, PostEdit, PostCreate } from '../PostList/PostList';
+import { PostList, PostEdit, PostCreate } from '../PostList/Post';
+import Dashboard from '../Dashboard/Dashboard'
+import authProvider from '../Auth/authProvider';
 import './App.css';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
-  <Admin dataProvider={dataProvider} >
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-    <Resource name="users" list={UserList} />
+  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} >
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 )
 
